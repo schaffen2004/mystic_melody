866777338a56c16505dff31d9a0de3bd0eb2ccb2
Phan Đức Nghĩa <123baconsau6969@gmail.com>
2023-06-14 22:41:35 +0700

    first

--- CMakeFiles/C.dir/DependInfo.cmake
+++ CMakeFiles/C.dir/DependInfo.cmake
@@ -1,50 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "C"
-  )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_C
-  "D:/mystic_melody/mystic_melody_v2.0/src/config/database_config.c" "D:/mystic_melody/mystic_melody_v2.0/CMakeFiles/C.dir/src/config/database_config.c.obj"
-  "D:/mystic_melody/mystic_melody_v2.0/src/control/main_board_control.c" "D:/mystic_melody/mystic_melody_v2.0/CMakeFiles/C.dir/src/control/main_board_control.c.obj"
-  "D:/mystic_melody/mystic_melody_v2.0/src/control/sign_in_control.c" "D:/mystic_melody/mystic_melody_v2.0/CMakeFiles/C.dir/src/control/sign_in_control.c.obj"
-  "D:/mystic_melody/mystic_melody_v2.0/src/control/sign_up_control.c" "D:/mystic_melody/mystic_melody_v2.0/CMakeFiles/C.dir/src/control/sign_up_control.c.obj"
-  "D:/mystic_melody/mystic_melody_v2.0/src/main.c" "D:/mystic_melody/mystic_melody_v2.0/CMakeFiles/C.dir/src/main.c.obj"
-  "D:/mystic_melody/mystic_melody_v2.0/src/models/playlist.c" "D:/mystic_melody/mystic_melody_v2.0/CMakeFiles/C.dir/src/models/playlist.c.obj"
-  "D:/mystic_melody/mystic_melody_v2.0/src/models/songs.c" "D:/mystic_melody/mystic_melody_v2.0/CMakeFiles/C.dir/src/models/songs.c.obj"
-  "D:/mystic_melody/mystic_melody_v2.0/src/models/users.c" "D:/mystic_melody/mystic_melody_v2.0/CMakeFiles/C.dir/src/models/users.c.obj"
-  "D:/mystic_melody/mystic_melody_v2.0/src/view/components/add_playlist.c" "D:/mystic_melody/mystic_melody_v2.0/CMakeFiles/C.dir/src/view/components/add_playlist.c.obj"
-  "D:/mystic_melody/mystic_melody_v2.0/src/view/components/menu.c" "D:/mystic_melody/mystic_melody_v2.0/CMakeFiles/C.dir/src/view/components/menu.c.obj"
-  "D:/mystic_melody/mystic_melody_v2.0/src/view/components/music_bar.c" "D:/mystic_melody/mystic_melody_v2.0/CMakeFiles/C.dir/src/view/components/music_bar.c.obj"
-  "D:/mystic_melody/mystic_melody_v2.0/src/view/components/search_bar.c" "D:/mystic_melody/mystic_melody_v2.0/CMakeFiles/C.dir/src/view/components/search_bar.c.obj"
-  "D:/mystic_melody/mystic_melody_v2.0/src/view/components/upload.c" "D:/mystic_melody/mystic_melody_v2.0/CMakeFiles/C.dir/src/view/components/upload.c.obj"
-  "D:/mystic_melody/mystic_melody_v2.0/src/view/intro_view.c" "D:/mystic_melody/mystic_melody_v2.0/CMakeFiles/C.dir/src/view/intro_view.c.obj"
-  "D:/mystic_melody/mystic_melody_v2.0/src/view/main_board_view.c" "D:/mystic_melody/mystic_melody_v2.0/CMakeFiles/C.dir/src/view/main_board_view.c.obj"
-  "D:/mystic_melody/mystic_melody_v2.0/src/view/sign_in_view.c" "D:/mystic_melody/mystic_melody_v2.0/CMakeFiles/C.dir/src/view/sign_in_view.c.obj"
-  "D:/mystic_melody/mystic_melody_v2.0/src/view/sign_up_view.c" "D:/mystic_melody/mystic_melody_v2.0/CMakeFiles/C.dir/src/view/sign_up_view.c.obj"
-  )
-set(CMAKE_C_COMPILER_ID "GNU")
-
-# Preprocessor definitions for this target.
-set(CMAKE_TARGET_DEFINITIONS_C
-  "LIBDEFLATE_DLL"
   )
 
-# The include file search paths:
-set(CMAKE_C_TARGET_INCLUDE_PATH
-  "."
-  "D:/msys64/msys64/mingw64/bin/../include/gtk-3.0"
-  "D:/msys64/msys64/mingw64/bin/../include/pango-1.0"
-  "D:/msys64/msys64/mingw64/bin/../include/glib-2.0"
-  "D:/msys64/msys64/mingw64/bin/../lib/glib-2.0/include"
-  "D:/msys64/msys64/mingw64/bin/../include/harfbuzz"
-  "D:/msys64/msys64/mingw64/bin/../include/freetype2"
-  "D:/msys64/msys64/mingw64/bin/../include/libpng16"
-  "D:/msys64/msys64/mingw64/bin/../include/fribidi"
-  "D:/msys64/msys64/mingw64/bin/../include/cairo"
-  "D:/msys64/msys64/mingw64/bin/../include/pixman-1"
-  "D:/msys64/msys64/mingw64/bin/../include/gdk-pixbuf-2.0"
-  "D:/msys64/msys64/mingw64/bin/../include/webp"
-  "D:/msys64/msys64/mingw64/bin/../include/atk-1.0"
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/codeN/mystic_melody/src/config/database_config.c" "CMakeFiles/C.dir/src/config/database_config.c.obj" "gcc" "CMakeFiles/C.dir/src/config/database_config.c.obj.d"
+  "C:/codeN/mystic_melody/src/control/main_board_control.c" "CMakeFiles/C.dir/src/control/main_board_control.c.obj" "gcc" "CMakeFiles/C.dir/src/control/main_board_control.c.obj.d"
+  "C:/codeN/mystic_melody/src/control/sign_in_control.c" "CMakeFiles/C.dir/src/control/sign_in_control.c.obj" "gcc" "CMakeFiles/C.dir/src/control/sign_in_control.c.obj.d"
+  "C:/codeN/mystic_melody/src/control/sign_up_control.c" "CMakeFiles/C.dir/src/control/sign_up_control.c.obj" "gcc" "CMakeFiles/C.dir/src/control/sign_up_control.c.obj.d"
+  "C:/codeN/mystic_melody/src/main.c" "CMakeFiles/C.dir/src/main.c.obj" "gcc" "CMakeFiles/C.dir/src/main.c.obj.d"
+  "C:/codeN/mystic_melody/src/models/playlist.c" "CMakeFiles/C.dir/src/models/playlist.c.obj" "gcc" "CMakeFiles/C.dir/src/models/playlist.c.obj.d"
+  "C:/codeN/mystic_melody/src/models/songs.c" "CMakeFiles/C.dir/src/models/songs.c.obj" "gcc" "CMakeFiles/C.dir/src/models/songs.c.obj.d"
+  "C:/codeN/mystic_melody/src/models/users.c" "CMakeFiles/C.dir/src/models/users.c.obj" "gcc" "CMakeFiles/C.dir/src/models/users.c.obj.d"
+  "C:/codeN/mystic_melody/src/view/components/add_playlist.c" "CMakeFiles/C.dir/src/view/components/add_playlist.c.obj" "gcc" "CMakeFiles/C.dir/src/view/components/add_playlist.c.obj.d"
+  "C:/codeN/mystic_melody/src/view/components/menu.c" "CMakeFiles/C.dir/src/view/components/menu.c.obj" "gcc" "CMakeFiles/C.dir/src/view/components/menu.c.obj.d"
+  "C:/codeN/mystic_melody/src/view/components/music_bar.c" "CMakeFiles/C.dir/src/view/components/music_bar.c.obj" "gcc" "CMakeFiles/C.dir/src/view/components/music_bar.c.obj.d"
+  "C:/codeN/mystic_melody/src/view/components/search_bar.c" "CMakeFiles/C.dir/src/view/components/search_bar.c.obj" "gcc" "CMakeFiles/C.dir/src/view/components/search_bar.c.obj.d"
+  "C:/codeN/mystic_melody/src/view/components/upload.c" "CMakeFiles/C.dir/src/view/components/upload.c.obj" "gcc" "CMakeFiles/C.dir/src/view/components/upload.c.obj.d"
+  "C:/codeN/mystic_melody/src/view/intro_view.c" "CMakeFiles/C.dir/src/view/intro_view.c.obj" "gcc" "CMakeFiles/C.dir/src/view/intro_view.c.obj.d"
+  "C:/codeN/mystic_melody/src/view/main_board_view.c" "CMakeFiles/C.dir/src/view/main_board_view.c.obj" "gcc" "CMakeFiles/C.dir/src/view/main_board_view.c.obj.d"
+  "C:/codeN/mystic_melody/src/view/sign_in_view.c" "CMakeFiles/C.dir/src/view/sign_in_view.c.obj" "gcc" "CMakeFiles/C.dir/src/view/sign_in_view.c.obj.d"
+  "C:/codeN/mystic_melody/src/view/sign_up_view.c" "CMakeFiles/C.dir/src/view/sign_up_view.c.obj" "gcc" "CMakeFiles/C.dir/src/view/sign_up_view.c.obj.d"
   )
 
 # Targets to which this target links.
